9127b49e41eba4db67ba283ad54fa04b0129de04
Sasha Abramowitz <reallysasha@gmail.com>
2018-05-10 21:48:28 +0200

    Added local networking with prototype players

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerActions))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float lookSens = 2f;
+    private PlayerActions actions;
+
+    void Start()
+    {
+        actions = GetComponent<PlayerActions>();
+    }
+
+    void Update()
+    {
+        // movement
+        var xMov = Input.GetAxis("Horizontal") * transform.right;
+        var yMov = Input.GetAxis("Vertical") * transform.forward;
+        var velocity = (xMov + yMov).normalized * speed;
+
+        actions.move(velocity);
+
+        // Rotation
+        var yRot = new Vector3(0, Input.GetAxis("Mouse X"), 0) * lookSens;
+        float xRot = Input.GetAxis("Mouse Y") * lookSens;
+
+        actions.rotate(yRot, xRot);
+    }
+}
---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    public Behaviour[] componentsToDisable;
+
+    private Camera mainCam;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            foreach (Behaviour comp in componentsToDisable)
+                comp.enabled = false;
+        }
+        else
+        {
+            // TODO move somewhere else
+            mainCam = Camera.main;
+            if (mainCam != null)
+                mainCam.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (mainCam != null)
+            mainCam.gameObject.SetActive(true);
+    }
+}
